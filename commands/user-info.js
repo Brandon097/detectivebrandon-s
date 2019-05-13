68ffe595862794ad91c7eb4c5d03c79ddd3f3e73
unknown <47625174+Brandon097@users.noreply.github.com>
2019-05-13 02:29:33 -0500

    fixed the problem in the command

--- commands/user-info.js
+++ commands/user-info.js
@@ -4,29 +4,29 @@ module.exports = {
 	"description": "Display info about yourself,\nplease mention yourself if you want to see what info is on your user account insteaf of just doing **user-info** otherwise it will cause and erorre that i haven't found a way to fix yet",
 	execute (message) {
 		if (!message.mentions.users.size) {
-			return message.channel.send(`Your username: ${message.author.username}
-			Your tag???: **${message.author.tag}**
-			Your ID: **${message.author.id}**
-			Your avatar: **${message.author.avatar}**
-			Your avatar link: **${message.author.avatarURL}**
-			Your default avatar link: **${message.author.defaultAvatarURL}**
-			Your default avatar link if you have one: **${message.author.displayAvatarURL}**
-			The last message you sent XD: ``${message.author.lastMessage}``
-			Discriminator???: **${message.author.discriminator}**
-			Account verified: **${message.author.verified}`);
+			return message.channel.send(`Your username: **${message.author.username}**
+Your tag???: **${message.author.tag}**
+Your user ID: **${message.author.id}**
+Your avatar: **${message.author.avatar}**
+Your avatar link: **${message.author.avatarURL}**
+Your default avatar link: **${message.author.defaultAvatarURL}**
+Your default avatar link if you have one: **${message.author.displayAvatarURL}**
+The last message you sent XD: __${message.author.lastMessage}__
+Discriminator???: **${message.author.discriminator}**
+Account verified: **${message.author.verified}**`);
 		}
 
 		const userList = message.mentions.users.map(user => `their username: **${user.username}**
-		their tag???: **${user.tag}**
-		their ID: **${user.id}**
-		their avatar: **${user.avatar}**
-		their avatar link: **${user.avatarURL}**
-		their default avatar link: **${user.defaultAvatarURL}**
-		their default avatar link if you have one: **${user.displayAvatarURL}**
-		are they bot: ${user.bot}**
-		The last message they've sent XD: ``${user.lastMessage}``
-		Discriminator???: **${user.discriminator}**
-		is User Account verified: **${user.verified}`);
+their tag???: **${user.tag}**
+their ID: **${user.id}**
+their avatar: **${user.avatar}**
+their avatar link: **${user.avatarURL}**
+their default avatar link: **${user.defaultAvatarURL}**
+their default avatar link if you have one: **${user.displayAvatarURL}**
+are they bot: **${user.bot}**
+The last message they've sent XD: __${user.lastMessage}__
+Discriminator???: **${user.discriminator}**
+is User Account verified: **${user.verified}**`);
 
 		message.channel.send(userList);
 	}
