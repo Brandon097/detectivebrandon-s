43112d1fd8701ed6077a793c47784403807b1e48
unknown <47625174+Brandon097@users.noreply.github.com>
2019-05-13 02:29:10 -0500

    fixed a problem in the comand

--- commands/server.js
+++ commands/server.js
@@ -4,18 +4,18 @@ module.exports = {
 	"description": "Display advanced info about this server.",
 	execute (message) {
 		message.channel.send(`Server name: **${message.guild.name}**
-		created by: ``${message.guild.owner}`` who lives in the country of: **${message.guild.region}**
-		 at: **${message.guild.createdAt}**
-		with icon**${message.guild.icon}**and **${message.guild.iconUrl}**
-		~~with population: **${message.guild.members}**
-		with number of towns: **${message.guild.channels}** the town hall is: **${message.guild.defaultChannel}**~~
-		afk channel: **${message.guild.afkChannel}** that kicks out a idle user in: **${message.guild.afkTimeout}**seconds
-		system channel: **${message.guild.systemChannel}**
-		~~number of roles: **${message.guild.roles}**~~
-		~~verficationLevel: **${message.guild.verficatonLevel}**~~
-		is server verified: **${message.guild.verified}**
-		is server large(250 members) or small: **${message.guild.large}**
-		required MFA level: **${message.guild.mfaLevel}**
-		 is explicit content filter enabled(scans and delete messages that contains explicit content): **${message.guild.explicitContentFilter}**`);
+created by:**${message.guild.owner}** who lives in the country of: **${message.guild.region}**
+ at: **${message.guild.createdAt}**
+  with icon: **${message.guild.icon}**and **${message.guild.iconUrl}**
+ ~~with population: **${message.guild.members}**
+    with number of towns: **${message.guild.channels}** the town hall is: **${message.guild.defaultChannel}**~~
+afk channel: **${message.guild.afkChannel}** that kicks out a idle user in: **${message.guild.afkTimeout}**seconds
+system channel: **${message.guild.systemChannel}**
+~~number of roles: **${message.guild.roles}**~~
+~~verficationLevel: **${message.guild.verficatonLevel}**~~
+is server verified: **${message.guild.verified}**
+is server large(250 members) or small: **${message.guild.large}**
+required MFA level: **${message.guild.mfaLevel}**
+is explicit content filter enabled(scans and delete messages that contains explicit content): **${message.guild.explicitContentFilter}**`);
 	}
 };
