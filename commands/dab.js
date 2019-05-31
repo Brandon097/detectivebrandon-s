65173ee67dd423dde4873076bd4d9ff7a203d850
unknown <47625174+Brandon097@users.noreply.github.com>
2019-05-31 06:21:32 -0500

    new dance buddy, set cooldown to 60 seconds

--- commands/dab.js
+++ commands/dab.js
@@ -2,9 +2,10 @@
 module.exports = {
 	"name": "dab",
 	"description": "dabbing",
-	"cooldown": 6,
+	"cooldown": 60,
 	execute (message) {
 		message.channel.send("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKQNx3wmKqNxZSu9Rqko-hzUInTJXCmsov1MT2fsktYOlgFIBO");
-		message.channel.send("https://tenor.com/view/sans-fortnite-default-dance-gif-13115851");
+		message.channel.send(`https://tenor.com/view/sans-fortnite-default-dance-gif-13115851
+		https://tenor.com/view/secure-contain-protect-scp-scp173-gif-14077238`);
 	}
 };
