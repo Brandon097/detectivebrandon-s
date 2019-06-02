22fc18d437ac122a9265e4ab22713bf36cd7a78a
unknown <47625174+Brandon097@users.noreply.github.com>
2019-06-02 12:47:37 -0500

    new Description for when account was created

--- commands/user-info.js
+++ commands/user-info.js
@@ -10,8 +10,9 @@ Your tag???: **${message.author.tag}**
 Your user ID: **${message.author.id}**
 Your avatar: **${message.author.avatar}**
 Your avatar link: **${message.author.avatarURL}**
-Your default avatar link: **${message.author.defaultAvatarURL}**
-Your default avatar link if you have one: **${message.author.displayAvatarURL}**
+the time when you first visited discord: **${message.author.createdAt}**
+with default avatar link: **${message.author.defaultAvatarURL}**
+and default avatar link if you have one: **${message.author.displayAvatarURL}**
 is you bot **${message.author.bot}**
 The last message you sent XD: __${message.author.lastMessage}__
 Discriminator???: **${message.author.discriminator}**
@@ -24,6 +25,7 @@ likes to be a\`\`${message.author.presence.game}\`\`model`);
 their tag???: **${user.tag}**
 their ID: **${user.id}**
 their avatar: **${user.avatar}**
+time they first visted discord: **${user.createdAt}**
 their avatar link: **${user.avatarURL}**
 their default avatar link: **${user.defaultAvatarURL}**
 their default avatar link if you have one: **${user.displayAvatarURL}**
@@ -32,7 +34,7 @@ The last message they've sent XD: __${user.lastMessage}__
 Discriminator???: **${user.discriminator}**
 \`\`\`other infomation\`\`\`
 current statues: \`\`${user.presence.status}\`\`
-likes to be a\`\`${user.presence.game}\`\`model, (if \`\`null\`\`=user is not playing a game)`);
+likes to be a\`\`${user.presence.game}\`\`model`);
 
 		message.channel.send(userList);
 	}
