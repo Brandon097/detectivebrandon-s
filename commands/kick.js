e28c966f1b897f87707f2a535f1ebe03d9210ab8
unknown <47625174+Brandon097@users.noreply.github.com>
2019-06-05 03:05:25 -0500

    took off guide only and added a line comment

--- commands/kick.js
+++ commands/kick.js
@@ -2,12 +2,12 @@
 module.exports = {
 	"name": 'kick',
 	"description": 'Tag a member and kick them',
-	"guildOnly": true,
 	execute (message) {
 		if (!message.mentions.users.size) {
 			return message.reply('you need to tag a user in order to kick them!');
 		}
-
+	// grab the "first" mentioned user from the message
+	// this will return a `User` object, just like `message.author`
 		const taggedUser = message.mentions.users.first();
 
         message.channel.send(`You wanted to kick: ${taggedUser.username}
