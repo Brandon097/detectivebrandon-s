69ba4d9f9dec2db1b836e51cc49bc306aa39d122
unknown <47625174+Brandon097@users.noreply.github.com>
2019-06-05 03:43:10 -0500

    removed a line of code since index.js line 109 already provides "you didn't provide any arguments"

--- commands/args-info.js
+++ commands/args-info.js
@@ -4,10 +4,6 @@ module.exports = {
 	"description": 'Information about the arguments provided.',
 	"args": true,
 	execute (message, args) {
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		}
-		message.channel.send(`Arguments: ${args}
-		Arguments length: ${args.length}`);
+		message.channel.send(`Arguments: ${args}\nArguments length: ${args.length}`);
 	}
 };
